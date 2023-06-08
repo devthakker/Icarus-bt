db523e3543a6ac203e0daec63b981ddfdf515f08
Devin Thakker <devin.thakker@outlook.com>
2023-06-08 00:27:57 -0500

    Removes params

--- BackTesting/riley_test.py
+++ BackTesting/riley_test.py
@@ -7,6 +7,6 @@ from strategy import BollingerBands
 riley = Riley()
 riley.set_cash(10000)
 riley.add_data_csv('HistoricalData/F.csv')
-riley.set_strategy(BollingerBands(20,1,2,2))
+riley.set_strategy(BollingerBands())
 riley.set_stake_quantity(5)
 riley.run()
