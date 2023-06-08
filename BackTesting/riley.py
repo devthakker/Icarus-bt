d7d17afd964606acf6aff1307f02f24bf0b208ad
Devin Thakker <devin.thakker@outlook.com>
2023-06-07 22:37:11 -0500

    two data methods

--- BackTesting/riley.py
+++ BackTesting/riley.py
@@ -3,11 +3,10 @@ import pandas as pd
 class Riley:
     
     def __init__(self, optimization=False) -> None:
+        self.optimization = optimization
         self.cash = None
         self.strategy = None
         self.data = None
-        self.optimization = optimization
-        self.strategy = None
         
     def set_cash(self, cash):
         self.cash = cash
@@ -17,9 +16,36 @@ class Riley:
         self.strategy = strategy
         return
     
-    def set_data(self, data):
+    def add_data_dataframe(self, data):
         if isinstance(data, pd.DataFrame):
-            self.data = data
+            df = pd.DataFrame(data)
+            if 'open' not in df.columns:
+                raise Exception('Data must contain an open column')
+            if 'high' not in df.columns:
+                raise Exception('Data must contain a high column')
+            if 'low' not in df.columns:
+                raise Exception('Data must contain a low column')
+            if 'close' not in df.columns:
+                raise Exception('Data must contain a close column')
+            self.data = df
         else:
-            self.data = pd.DataFrame(data)
+            raise Exception('Data must be a pandas dataframe')
         return
+    
+    def add_data_csv(self, path):
+        if isinstance(path, str):
+            df = pd.read_csv(path)
+            if 'open' not in df.columns:
+                raise Exception('Data must contain an open column')
+            if 'high' not in df.columns:
+                raise Exception('Data must contain a high column')
+            if 'low' not in df.columns:
+                raise Exception('Data must contain a low column')
+            if 'close' not in df.columns:
+                raise Exception('Data must contain a close column')
+            self.data = df
+        else:
+            raise Exception('Path is invalid')
+        return
+    
+    
