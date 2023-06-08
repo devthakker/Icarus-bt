0b6c2d482c9da5201707e37dd2c2dc4458b9eb06
Devin Thakker <devin.thakker@outlook.com>
2023-06-07 22:32:45 -0500

    Set methods

--- BackTesting/riley.py
+++ BackTesting/riley.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 class Riley:
     
@@ -5,3 +6,20 @@ class Riley:
         self.cash = None
         self.strategy = None
         self.data = None
+        self.optimization = optimization
+        self.strategy = None
+        
+    def set_cash(self, cash):
+        self.cash = cash
+        return
+    
+    def set_strategy(self, strategy):
+        self.strategy = strategy
+        return
+    
+    def set_data(self, data):
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        else:
+            self.data = pd.DataFrame(data)
+        return
