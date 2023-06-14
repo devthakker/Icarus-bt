77151fcf9162ecda4c8afc04500e032f1da86447
Devin Thakker <devin.thakker@outlook.com>
2023-06-13 23:17:45 -0500

    metrics

---
+++ Icarus/metrics/annualizedreturns.py
@@ -0,0 +1,9 @@
+class AnnualizedReturn:
+    def __init__(self, returns):
+        self.returns = returns
+
+    def calculate(self):
+        num_periods = len(self.returns)
+        total_return = (1 + sum(self.returns)) ** (1 / num_periods) - 1
+        annualized_return = (1 + total_return) ** 252 - 1  # Assuming 252 trading days in a year
+        return annualized_return
---
+++ Icarus/metrics/totalreturn.py
@@ -0,0 +1,8 @@
+class TotalReturn:
+    def __init__(self, initial_value, final_value):
+        self.initial_value = initial_value
+        self.final_value = final_value
+
+    def calculate(self):
+        total_return = (self.final_value - self.initial_value) / self.initial_value
+        return total_return
