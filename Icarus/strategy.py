abffce869fa0a11af38ed0c2fd2f07792dd4ca0a
Devin Thakker <devin.thakker@outlook.com>
2023-06-25 22:49:43 -0500

    source tests

--- Icarus/strategy.py
+++ Icarus/strategy.py
@@ -1,12 +1,14 @@
-
+import logging
 
 class strategy:
     """
     Base class for all strategies.
     """
-    def __init__(self, optimize=False, optimize_range=range(1, 20)):
+    def __init__(self, optimize=False, optimize_range=range(1, 20), logging=False):
         self.optimize = optimize
         self.optimize_range = optimize_range
+        if logging:
+            self.logger = logging.getLogger(__name__)
 
     def check_for_buy(self, line):
         pass
